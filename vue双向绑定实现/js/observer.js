3466032f851cf63e2d0720d511ba2923df093751
Mohan <1063022109@qq.com>
2019-06-17 10:38:02 +0800

    vue双向绑定

---
+++ vue双向绑定实现/js/observer.js
@@ -0,0 +1,60 @@
+function Observer(data) {
+    this.data = data;
+    this.walk(data);
+}
+
+Observer.prototype = {
+    walk: function(data) {
+        var self = this;
+        Object.keys(data).forEach(function(key) {
+            self.defineReactive(data, key, data[key]);
+        });
+    },
+    defineReactive: function(data, key, val) {
+        var dep = new Dep();
+        // var childObj = observe(val);
+        Object.defineProperty(data, key, {
+            enumerable: true,
+            configurable: true,
+            get: function getter() {
+                if (Dep.target) {
+                    dep.addSub(Dep.target);
+                }
+                console.log(val)
+                return val;
+            },
+            set: function setter(newVal) {
+                // console.log(dep)
+                if (newVal === val) {
+                    return;
+                }
+                // console.log(newVal)
+                val = newVal;
+                dep.notify();
+            }
+        });
+    }
+};
+
+function observe(value, vm) {
+    if (!value || typeof value !== 'object') {
+        return;
+    }
+    return new Observer(value);
+};
+
+function Dep() {
+    this.subs = [];
+}
+Dep.prototype = {
+    addSub: function(sub) {
+        this.subs.push(sub);
+    },
+    notify: function() {
+        this.subs.forEach(function(sub) {
+            // console.log(sub)
+            sub.update();
+        });
+    }
+};
+Dep.target = null;
